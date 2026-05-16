e7a9254204f463fa648e5cf4d1eaa357bff0c701
jose-pc <josegu05@ucm.es>
2026-05-16 19:09:08 +0200

    restructuracion inicial

---
+++ inc/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN__
+#define MAIN__
+
+void init_rcc_configuration (void);
+#endif
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void) {
+
+   init_rcc_configuration();
+   while (1);
+}
+
+void init_rcc_configuration (void)
+{
+}
