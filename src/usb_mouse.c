c6ba29d102839d01f40f94098c7930d7381fae64
jose-pc <josegu05@ucm.es>
2026-06-01 21:23:01 +0200

    added descriptors
    CONT: hid_descriptor incompleto. mirar como funciona el driver
    
    added endpoint descriptor.
    added hid_report descriptor.
    
    CONT: check commits before

--- src/usb_mouse.c
+++ src/usb_mouse.c
@@ -60,6 +60,42 @@ static const struct usb_hid_descriptor_full rj_hid_descriptor {
 
 };
 
+static const struct usb_endpoint_descriptor rj_endpoint_descriptor {
+      .bLength = USB_DT_ENDPOINT_SIZE,
+      .bDescriptorType = USB_DT_ENDPOINT,
+      .bEndpointAddress = USB_ENDPOINT_ADDR_IN(1),
+      .bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
+      .wMaxPacketSize = 4,
+      .bInterval = 10,
+}
+
+static const uint8_t rj_hid_report_descriptor[] = {
+	0x05, 0x01, /* USAGE_PAGE (Generic Desktop)         */
+	0x09, 0x02, /* USAGE (Mouse)                        */
+	0xa1, 0x01, /* COLLECTION (Application)             */
+	0x09, 0x01, /*   USAGE (Pointer)                    */
+	0xa1, 0x00, /*   COLLECTION (Physical)              */
+	0x05, 0x09, /*     USAGE_PAGE (Button)              */
+	0x19, 0x01, /*     USAGE_MINIMUM (Button 1)         */
+	0x29, 0x03, /*     USAGE_MAXIMUM (Button 3)         */
+	0x15, 0x00, /*     LOGICAL_MINIMUM (0)              */
+	0x25, 0x01, /*     LOGICAL_MAXIMUM (1)              */
+	0x95, 0x08, /*     REPORT_COUNT (8)                 */
+	0x75, 0x01, /*     REPORT_SIZE (1)                  */
+	0x81, 0x02, /*     INPUT (Data,Var,Abs)             */
+	0x05, 0x01, /*     USAGE_PAGE (Generic Desktop)     */
+	0x09, 0x30, /*     USAGE (X)                        */
+	0x09, 0x31, /*     USAGE (Y)                        */
+	0x09, 0x38, /*     USAGE (Wheel)                    */
+	0x15, 0x81, /*     LOGICAL_MINIMUM (-127)           */
+	0x25, 0x7f, /*     LOGICAL_MAXIMUM (127)            */
+	0x75, 0x08, /*     REPORT_SIZE (8)                  */
+	0x95, 0x03, /*     REPORT_COUNT (3)                 */
+	0x81, 0x06, /*     INPUT (Data,Var,Rel)             */
+	0xc0,       /*   END_COLLECTION                     */
+	0xc0        /* END_COLLECTION                       */
+};
+
 // ----------- BEGIN DECLS ------------
 
 void usb_rj_init (void)
