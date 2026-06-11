e85276b7d2541f7edb700cbb9059b8a60cdef525
jose-pc <josegu05@ucm.es>
2026-06-11 21:10:54 +0200

    Add config functions y hid
    Compilado y captura asociada. No funciona observar captura para
    ver en que descriptores falla.
    
    check commits below. And notebook!
    cont backtrace: st_usbfs_poll, st_usbfs_ep_read_packet
    cont paralell: usbd_register_set_config_callback
    cont paralell: _usbd_control_setup at libopencm3/lib/usb/usb_control.c

--- inc/usb_rj_mouse.h
+++ inc/usb_rj_mouse.h
@@ -2,13 +2,14 @@
 #define _RJ_USB_H_
 
 #include <stdint.h>
+#include <stddef.h>
 #include <libopencm3/usb/hid.h>
 #include <libopencm3/usb/usbd.h>
 
 #define OPEN_SOURCE_VID 0x1209
 #define TEST_PID 0x1
 #define N_USB_STRINGS 2
-#define USB_HID_DESCRIPTOR_SIZE sizeof(struct usb_complete_hid_descriptor)
+#define USB_HID_DESCRIPTOR_SIZE 9
 
 struct usb_hid_report_fields {
    uint8_t bDescriptorType;
@@ -19,6 +20,7 @@ struct usb_complete_hid_descriptor {
    struct usb_hid_descriptor hid_descriptor_fields;
    struct usb_hid_report_fields hid_report_fields;
 } __attribute__((packed));
+
 
 // BEGIN DECLS -----------
 void usb_rj_init (void);
--- src/usb_rj_mouse.c
+++ src/usb_rj_mouse.c
@@ -4,6 +4,10 @@
 
 static usbd_device * rj_dev_p = 0;
 static uint8_t rj_control_buffer[128];
+static const struct usb_complete_hid_descriptor rj_complete_hid_descriptor;
+static void rj_config_setup (usbd_device *dev, uint16_t wValue);
+static enum usbd_request_return_codes rj_hid_control_request(usbd_device *dev, struct usb_setup_data *req, uint8_t **buf, uint16_t *len,
+			void (**complete)(usbd_device *, struct usb_setup_data *));
 
 static const struct usb_device_descriptor rj_device_descriptor = {
 	.bLength = USB_DT_DEVICE_SIZE,
@@ -31,7 +35,7 @@ static const char *rj_strings[N_USB_STRINGS] = {
 static const struct usb_config_descriptor rj_config_descriptor = {
        .bLength = USB_DT_CONFIGURATION_SIZE,
        .bDescriptorType = USB_DT_CONFIGURATION,
-       .wTotalLength = 0, //TODO: Calculate total configuration length in Bytes EP + Interfaces ..
+       .wTotalLength = (USB_DT_CONFIGURATION_SIZE + USB_DT_INTERFACE_SIZE + USB_HID_DESCRIPTOR_SIZE + USB_DT_ENDPOINT_SIZE),
        .bNumInterfaces = 1,
        .bConfigurationValue = 1,
        .iConfiguration = 0,
@@ -51,8 +55,8 @@ static const struct usb_interface_descriptor rj_interface_descriptor = {
       .iInterface = 0,
 
 //    .endpoint = &hid_endpoint,
-//    .extra = &hid_function,
-//    .extralen = sizeof(hid_function),
+      .extra = &rj_complete_hid_descriptor,
+      .extralen = sizeof(rj_complete_hid_descriptor),
 
 };
 
@@ -64,13 +68,23 @@ static const struct usb_hid_descriptor rj_hid_descriptor = {
       .bNumDescriptors = 1,
 };
 
+static const struct usb_complete_hid_descriptor rj_complete_hid_descriptor = 
+{
+   .hid_descriptor_fields = rj_hid_descriptor,
+   .hid_report_fields =
+   {
+      .bDescriptorType = USB_HID_DT_REPORT,
+      .wDescriptorLength = sizeof(rj_hid_descriptor)
+   }
+};
+
 static const struct usb_endpoint_descriptor rj_endpoint_descriptor = {
       .bLength = USB_DT_ENDPOINT_SIZE,
       .bDescriptorType = USB_DT_ENDPOINT,
       .bEndpointAddress = USB_ENDPOINT_ADDR_IN(1),
       .bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
       .wMaxPacketSize = 4,
-      .bInterval = 10,
+      .bInterval = 50,
 };
 
 static const uint8_t rj_hid_report_descriptor[] = {
@@ -100,24 +114,51 @@ static const uint8_t rj_hid_report_descriptor[] = {
 	0xc0        /* END_COLLECTION                       */
 };
 
-static const struct usb_complete_hid_descriptor rj_complete_hid_descriptor = 
-{
-   .hid_descriptor_fields = rj_hid_descriptor,
-   .hid_report_fields =
-   {
-      .bDescriptorType = USB_HID_DT_REPORT,
-      .wDescriptorLength = sizeof(rj_hid_descriptor)
-   }
-};
 
 // ----------- BEGIN DECLS ------------
 
 void usb_rj_init (void)
 {
    rj_dev_p = usbd_init(&st_usbfs_v1_usb_driver, &rj_device_descriptor, &rj_config_descriptor, rj_strings, N_USB_STRINGS, rj_control_buffer, sizeof(rj_control_buffer));
+
+   usbd_register_set_config_callback(rj_dev_p, rj_config_setup);
 }
 
 void usb_rj_poll (void)
 {
    usbd_poll(rj_dev_p);
 }
+
+static void rj_config_setup (usbd_device *dev, uint16_t wValue)
+{
+
+   (void)wValue;
+   (void)dev;
+
+   usbd_ep_setup(dev, USB_ENDPOINT_ADDR_IN(1), USB_ENDPOINT_ATTR_INTERRUPT, 4, NULL);
+
+   usbd_register_control_callback(
+		       dev,
+		       USB_REQ_TYPE_STANDARD | USB_REQ_TYPE_INTERFACE,
+		       USB_REQ_TYPE_TYPE | USB_REQ_TYPE_RECIPIENT,
+		       rj_hid_control_request);
+}
+
+static enum usbd_request_return_codes rj_hid_control_request(usbd_device *dev, struct usb_setup_data *req, uint8_t **buf, uint16_t *len,
+			void (**complete)(usbd_device *, struct usb_setup_data *))
+{
+	(void)complete;
+	(void)dev;
+
+	if((req->bmRequestType != 0x81) ||
+	   (req->bRequest != USB_REQ_GET_DESCRIPTOR) ||
+	   (req->wValue != 0x2200))
+		return USBD_REQ_NOTSUPP;
+
+	/* Handle the HID report descriptor. */
+	*buf = (uint8_t *)rj_hid_report_descriptor;
+	*len = sizeof(rj_hid_report_descriptor);
+
+	return USBD_REQ_HANDLED;
+}
+
